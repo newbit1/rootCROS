110c9d5de26e9074c5d400f11982b482ea8abc51
NewBit <newbit1@github.com>
2021-05-22 17:33:07 +0200

    updates

--- rootCROS.sh
+++ rootCROS.sh
@@ -10,6 +10,11 @@ GainRoot() {
 	  #echo "run:"
 	  #echo "cd \$HOME && sudo curl -LO https://raw.githubusercontent.com/newbit1/rootCROS/master/rootCROS.sh && sudo chmod +x && bash ./rootCROS.sh"
 	  #echo "sudo bash ./rootCROS.sh"
+	  #echo "restore0=$restore"
+	  #if [ ! -z $restore ]; then
+	#		export restore
+	 # fi
+	  #export restore
 	  sudo bash -c "exec bash $0 $@"
 	  exit 0
 	fi
@@ -26,9 +31,11 @@ ChangeLocation() {
 	
 	if [ "$CURDIR" != "$BASEDIR" ]; then
 		echo "[-] Moving to the location $BASEDIR"
+		#rm $BASEDIR/*
 		cp "$0" $BASEDIR
 		cd $BASEDIR
 		echo "[*] Re-Execute the script proper"
+		echo "restore1=$restore"
 		bash -c "exec ./$0 $@"
 		exit 0
 	fi
@@ -41,6 +48,10 @@ ProcessArguments() {
 	RemountDrive=false
 	CleanUpMounts=false
 	
+	if [ -z $restore ]; then
+		restore=false
+	fi
+	restore=true
 	# Overlay Directorys
 	FM=$BASEDIR/FM
 	TO=$BASEDIR/to
@@ -52,6 +63,7 @@ ProcessArguments() {
 	
 	ANDROIROOTDIR=/opt/google/containers/android/rootfs/root
 	SYSRAWIMG=/opt/google/containers/android/system.raw.img
+	VENRAWIMG=/opt/google/containers/android/vendor.raw.img
 	
 	#RECOVERYIMG=/home/$USER/Downloads/chromeos_13816.64.0_rammus_recovery_stable-channel_mp-v2.bin.img
 	# ROOT-A contains the android container system and vendor
@@ -59,7 +71,7 @@ ProcessArguments() {
 	#ROOTA=/media/newbit/ROOT-A
 	
 		
-	#ANDROIDATADIR=/opt/google/containers/android/rootfs/android-data
+	ANDROIDATADIR=/opt/google/containers/android/rootfs/android-data
 	ADBWORKDIR=/data/data/com.android.shell
 	ADBBASEDIR=$ADBWORKDIR/Magisk
 	
@@ -71,6 +83,11 @@ ProcessArguments() {
 		RemountDrive=true
 	fi
 
+	if [[ "$@" == *"restore"* ]]; then
+		restore=true
+	fi
+	
+	echo "at0=$@"
 	export FM
 	export TO
 	export TEMP
@@ -80,13 +97,16 @@ ProcessArguments() {
 	export TMPDIR
 	
 	export ANDROIROOTDIR
+	export ANDROIDATADIR
 	export SYSRAWIMG
+	export VENRAWIMG
 
 	export ADBWORKDIR
 	export ADBBASEDIR
 	
 	export RemountDrive
 	export CleanUpMounts
+	export restore
 }
 
 DownloadAssets() {
@@ -113,6 +133,7 @@ DownloadAssets() {
 	if [ ! -e "$TARGET" ]; then
 		echo "[-] Downloading $TARGET"
 		curl -# -L https://github.com/Magisk-Modules-Repo/busybox-ndk/raw/master/$TARGET-x86_64 -o $TARGET && chmod +x $TARGET
+		#curl -# -L https://busybox.net/downloads/binaries/1.31.0-defconfig-multiarch-musl/busybox-i686 -o $TARGET && chmod +x $TARGET		
 	else
 		echo "[-] $TARGET already there"
 	fi
@@ -140,12 +161,30 @@ CreateFakeRamdisk() {
 	export RAMDISKDIR
 }
 
+InitADB() {
+	echo "[*] Init ADB"
+	ADBWORKS=$(adb shell 'echo true' 2>/dev/null)&
+	pids=" $!"
+	wait $pids
+
+	if [[ "$ADBWORKS" == *"ADB_VENDOR_KEYS"* ]]; then
+		adb kill-server &
+		pids=" $!"
+		wait $pids
+		adb shell &
+		pids=" $!"
+		wait $pids
+	fi
+}
+
 PatchFakeRamdisk() {
 	echo "[*] Cleaning up the ADB working space"
 	adb shell rm -rf $ADBBASEDIR
 	echo "[*] Creating the ADB working space"
 	adb shell mkdir $ADBBASEDIR
-	adb push * $ADBBASEDIR
+	adb push rootAVD.sh $ADBBASEDIR
+	adb push ramdisk.img $ADBBASEDIR
+	adb push Magisk.zip $ADBBASEDIR
 	adb shell sh $ADBBASEDIR/rootAVD.sh $@	
 	#$ROOTAVD $BASEDIR/ramdisk.img
 	adb pull $ADBBASEDIR/ramdiskpatched4AVD.img $BASEDIR/ramdisk.img
@@ -176,11 +215,6 @@ CleanUpMounts() {
 	CleanUp $TO
 	CleanUp $TEMP
 	CleanUp $FIN
-	#CleanUp $MAGISKDIR
-	#CleanUp $BB
-	#for file in magisk*; do
-	#	CleanUp $file
-	#done
 }
 
 CreateOverlayMounts() {
@@ -188,8 +222,13 @@ CreateOverlayMounts() {
 	mkdir $TO
 	mkdir $TEMP
 	mkdir $FIN
-	mount $SYSRAWIMG $FM -t squashfs -o loop
-	mount -t overlay -o lowerdir=$FM,upperdir=$TO,workdir=$TEMP overlay $FIN
+	#-o context=system_u:object_r:public_content_t:s0 /dev/sdb1
+	#drwxr-xr-x. 17 android-root android-root u:object_r:rootfs:s0 909 May 17 09:21 /opt/google/containers/android/rootfs/root/
+	#mount $SYSRAWIMG $FM -t squashfs -o loop
+	#mount -o remount,nosuid,nodev,noexec $FM
+	#mount -t overlay -o lowerdir=$FM,upperdir=$TO,workdir=$TEMP overlay $FIN
+	#overlay on /usr/local/crosswork/fin type overlay (rw,relatime,lowerdir=/usr/local/crosswork/FM,upperdir=/usr/local/crosswork/to,workdir=/usr/local/crosswork/temp)
+	unsquashfs -f -d $FIN $SYSRAWIMG
 }
 
 MountAndroidRaws() {
@@ -233,35 +272,135 @@ ReadContextPerm() {
 
 SetPerm() {
 	# SET PERM from $1 to $2
-	echo "[*] Set Permissions and Context from $1 to $2"
+	echo "[!] Set Permissions and Context from"
+	echo "[-] $1 to $2"
+	echo "[*] Change Owner:Group to $(stat -c %u $1):$(stat -c %g $1)"	
+	chown $(stat -c %u $1):$(stat -c %g $1) $2
+	echo "[-] Change Mod to $(stat -c %#a $1)"	
+	chmod $(stat -c %#a $1) $2
+	echo "[*] Change Context to $(stat -c %C $1)"
 	chcon --reference=$1 $2	
-	chown $(sudo stat -c %u $1):$(sudo stat -c %g $1) $2
-	chmod $(sudo stat -c %#a $1) $2	
+}
+
+SetOwner() {
+	# SET OWNER from $1 to $2
+	chown $(stat -c %u $1):$(stat -c %g $1) $2
+}
+
+patch_init() {
+
+	echo "[*] Injecting Magisk into init.rc"
+
+	echo "
+on post-fs-data
+    start logd
+    rm /dev/.magisk_unblock
+    start QOE79THp1LNiWLP
+    wait /dev/.magisk_unblock 40
+    rm /dev/.magisk_unblock
+
+service QOE79THp1LNiWLP /sbin/magisk --post-fs-data
+    user root
+    seclabel u:r:magisk:s0
+    oneshot
+
+service magiskdaemon /sbin/magisk --daemon
+    user root
+    seclabel u:r:magisk:s0
+    oneshot
+    
+service magiskpolicy /sbin/magiskpolicy --live --magisk
+    user root
+    seclabel u:r:magisk:s0
+    oneshot
+
+service q2RZ4jzDFsBXQT7 /sbin/magisk --service
+    class late_start
+    user root
+    seclabel u:r:magisk:s0
+    oneshot
+
+on property:sys.boot_completed=1
+    start KILQmKFSZy
+
+service KILQmKFSZy /sbin/magisk --boot-complete
+    user root
+    seclabel u:r:magisk:s0
+    oneshot
+" >>  $FIN/init.rc
 }
 
 PatchOverlayWithFakeRamdisk() {
 	echo "[*] Patching Overlay with fake ramdisk.img"
 	mv $BASEDIR/ramdisk.img $BASEDIR/ramdisk.cpio.gz
-	$BB gzip -d $BASEDIR/ramdisk.cpio.gz
-	#mkdir -p $RAMDISKDIR
-	echo "[-] Extracting ramdisk.cpio to overlay System"
-	cd $FIN > /dev/null
+	$BB gzip -fd $BASEDIR/ramdisk.cpio.gz
+	mkdir -p $RAMDISKDIR
+	#set -x
+	echo "[-] Extracting ramdisk.cpio"
+	#cd $FIN > /dev/null
+	cd $RAMDISKDIR > /dev/null
 		cat $BASEDIR/ramdisk.cpio | $BB cpio -i > /dev/null 2>&1
 	cd - > /dev/null
-
-	SetPerm $ANDROIROOTDIR/init $FIN/init
 	
-	#chcon --reference=/opt/google/containers/android/rootfs/root/init /usr/local/crosswork/fin/init
-
-	#SetPerm $FIN/sbin $FIN/.backup
-	#chmod 000 -R $FIN/.backup
-	stat -c %C%u%g%#a $ANDROIROOTDIR/init
-	stat -c %C%u%g%#a $FIN/init
-
-	SetPerm $ANDROIROOTDIR/sbin $FIN/overlay.d
-	SetPerm $ANDROIROOTDIR/sbin $FIN/overlay.d/sbin
-	SetPerm $ANDROIROOTDIR/sbin $FIN/overlay.d/sbin/magisk32.xz
-	SetPerm $ANDROIROOTDIR/sbin $FIN/overlay.d/sbin/magisk64.xz
+	mv $RAMDISKDIR/init $RAMDISKDIR/overlay.d/sbin/magiskinit
+	
+	echo "[*] Copy Ramdisk Files to /sbin"
+	cp -r $RAMDISKDIR/overlay.d/sbin/* $FIN/sbin/
+	
+	cd $FIN/sbin > /dev/null
+		$BB unxz magisk64.xz
+		$BB unxz magisk32.xz
+		#chcon u:object_r:magisk_exec:s0 ./magisk64
+		chcon u:object_r:system_file:s0 ./magisk64
+		chcon u:object_r:system_file:s0 ./magisk32
+		
+		SetOwner $FIN/init ./magisk64
+		SetOwner $FIN/init ./magisk32		
+		chmod 0755 ./magisk64
+		chmod 0755 ./magisk32
+		
+		#ln -s ./magisk32 ./magisk
+		ln -sf ./magisk64 ./magisk
+		ln -sf ./magisk ./su
+		ln -sf ./magisk ./resetprop
+		ln -sf ./magisk ./magiskhide
+		
+		#SetOwner $FIN/init ./magisk
+		#SetOwner $FIN/init ./su
+		#SetOwner $FIN/init ./resetprop
+		#SetOwner $FIN/init ./magiskhide
+		
+		chcon u:object_r:system_file:s0 ./magiskinit
+		SetOwner $FIN/init ./magiskinit
+		chmod 0755 ./magiskinit
+		ln -sf ./magiskinit ./magiskpolicy
+	cd - > /dev/null
+	
+	mkdir -p $ANDROIDATADIR/data/adb/magisk
+	mkdir -p $ANDROIDATADIR/data/adb/modules
+	mkdir -p $ANDROIDATADIR/data/adb/post-fs-data.d
+	mkdir -p $ANDROIDATADIR/data/adb/services.d
+	
+	cd $ANDROIDATADIR/data/adb > /dev/null
+		#chcon u:object_r:magisk_file:s0 ./magisk
+		chcon u:object_r:system_file:s0 ./magisk
+		chcon u:object_r:system_file:s0 ./modules
+		chcon u:object_r:adb_data_file:s0 ./post-fs-data.d
+		chcon u:object_r:adb_data_file:s0 ./services.d
+		
+		SetOwner $ANDROIDATADIR/data/adb ./magisk
+		SetOwner $ANDROIDATADIR/data/adb ./modules
+		SetOwner $ANDROIDATADIR/data/adb ./post-fs-data.d
+		SetOwner $ANDROIDATADIR/data/adb ./services.d
+		
+		chmod 0755 ./magisk
+		chmod 0755 ./modules
+		chmod 0755 ./post-fs-data.d
+		chmod 0755 ./services.d				
+	cd - > /dev/null
+	
+	patch_init
+	#cat $FIN/init.rc
 }
 
 makeSQUASHFS() {
@@ -287,7 +426,26 @@ create_backup() {
 setMagiskSQUASHFStoSYSTEM() {
 	echo "[*] Set Magisk SquashFS System"
 	mv $SYSRAWIMG.magisk $SYSRAWIMG
+	echo "[*] Change Context to $(stat -c %C $VENRAWIMG)"
+	chcon --reference=$VENRAWIMG $SYSRAWIMG
 }
+
+restore_backup() {
+	local BACKUPFILE=""
+	local FILE=""
+	FILE="$1"
+	BACKUPFILE="$FILE.backup"
+
+	if [ -e "$BACKUPFILE" ]; then
+		echo "[*] Restore Backup File of $FILE"
+		rm $SYSRAWIMG
+		mv $BACKUPFILE $SYSRAWIMG
+		chcon --reference=$VENRAWIMG $SYSRAWIMG
+	else
+		echo "[!] No Backup to restore"
+	fi
+}
+
 
 GainRoot
 ChangeLocation
@@ -296,11 +454,16 @@ ProcessArguments $@
 #####
 $RemountDrive && RemountDrive && exit 0
 $CleanUpMounts && CleanUpMounts && exit 0
+$restore && RemountDrive && restore_backup $SYSRAWIMG && exit 0
 #####
 
 DownloadAssets
-CreateFakeRamdisk
-PatchFakeRamdisk
+
+if [ ! -e "$BASEDIR/ramdisk.img" ]; then
+	InitADB
+	CreateFakeRamdisk
+	PatchFakeRamdisk
+fi
 
 CleanUpMounts
 RemountDrive
@@ -313,7 +476,8 @@ makeSQUASHFS
 create_backup $SYSRAWIMG
 setMagiskSQUASHFStoSYSTEM
 CleanUpMounts
-
+rm -r $BASEDIR/ramdisk.cpio > /dev/null 2>&1 &
+rm -r $TMPDIR > /dev/null 2>&1 &
 exit 0
 
 #ReadContextPerm
@@ -335,5 +499,26 @@ sudo semanage fcontext -a -e /media/$USER/fin/init.rc /media/$USER/fin/init
 
 sudo mv /media/$USER/ROOT-A1/opt/google/containers/android/system.raw.img /media/$USER/ROOT-A1/opt/google/containers/android/system.raw.img.bak
 sudo mv /media/$USER/ROOT-A1/opt/google/containers/android/system.raw.img.magisk /media/$USER/ROOT-A1/opt/google/containers/android/system.raw.img
+
+	# Magisk stuffs
+	#xsymlink("./magisk64", "magisk");
+	#cp -af ./magisk $BINDIR/magisk
+	#chmod 755 $BINDIR/magisk
+	#ln -s ./magisk $BINDIR/su
+	#ln -s ./magisk $BINDIR/resetprop
+	#ln -s ./magisk $BINDIR/magiskhide
+	#ln -sf ./magiskinit magiskpolicy
+	#./magiskpolicy --live --magisk
+	#mkdir -p /data/adb/modules 2>/dev/null
+	#mkdir /data/adb/post-fs-data.d 2>/dev/null
+	#mkdir /data/adb/services.d 2>/dev/null
+	#$BINDIR/magisk --daemon
+
+	#SetPerm $ANDROIROOTDIR/init $RAMDISKDIR/init
+	#SetPerm $ANDROIROOTDIR/sbin $RAMDISKDIR/overlay.d
+	#SetPerm $ANDROIROOTDIR/sbin $RAMDISKDIR/overlay.d/sbin
+	#SetPerm $ANDROIROOTDIR/sbin $RAMDISKDIR/overlay.d/sbin/magisk32.xz
+	#SetPerm $ANDROIROOTDIR/sbin $RAMDISKDIR/overlay.d/sbin/magisk64.xz	
+	#cp -r --preserve=context $RAMDISKDIR/* $FIN/
 
 exit
